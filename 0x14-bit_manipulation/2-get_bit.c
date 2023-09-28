74f10bb5ed9919e79f8a5d1de672fb32e7cec8e6
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-09-28 17:43:21 +0300

    Task 2.10

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: decimal number
+ * @index: index
+ * Return: (index)th value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int buffer[100], i = 0;
+
+	if (n == 0 && index == 0)
+		return (0);
+
+	i = log(n + 1) / log(2);
+	if (index > i)
+		return (-1);
+
+	i = 0;
+	while (n > 0)
+	{
+		if (n & 1)
+			buffer[i] = 1;
+		else
+			buffer[i] = 0;
+		i++;
+		n = n >> 1;
+	}
+	return (buffer[index]);
+}
