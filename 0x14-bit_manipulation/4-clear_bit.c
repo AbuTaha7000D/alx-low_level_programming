8f097c7a5a3e5cf9553c3a57705cda2691819f46
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-09-28 21:42:38 +0300

    Task 4.100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: Address of the number
+ * @index: index
+ *
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int jocker = 1;
+
+	if (index > sizeof(*n) * 8)
+		return (-1);
+	if (*n == 0)
+		return (1);
+	jocker <<= index;
+	*n = (*n ^ jocker);
+	return (1);
+}
