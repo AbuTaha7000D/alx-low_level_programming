8ff202a12835ccf8b0cf3cae80ff3e46c1d93702
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-09-28 15:42:20 +0300

    task 1.0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	int	len = 0, i = 1, num = 0, temp;
+	
+	if (!b)
+		return (0);
+	while (b[len])
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		len++;
+	}
+	while (len--)
+	{
+		temp = b[len] - '0';
+		num += (temp * i);
+		i *= 2;
+	}
+	return (num);
+}
+
