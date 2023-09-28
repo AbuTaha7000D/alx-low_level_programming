6f737c06c48bc34a0216a1409de736189d8d79e2
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-09-28 15:46:59 +0300

    task 0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * binary_to_uint - convert bin number to decimal
+ * @b: binary number
+ *
+ * Return: decimal value
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	int	len = 0, i = 1, num = 0, temp;
-	
+
 	if (!b)
 		return (0);
 	while (b[len])
